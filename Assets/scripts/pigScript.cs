1a3514513875944be747abd76146f25e741e6633
Slava <sh-e-95@mail.ru>
2021-07-22 19:44:15 +0500

    add pig script

--- Assets/scripts/pigScript.cs
+++ Assets/scripts/pigScript.cs
@@ -1,18 +1,74 @@
-using System.Collections;
+
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class pigScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int Health;
+    public int Armor;
+    public List<Sprite> DefaultPig;
+    public List<Sprite> DamagedPig;
+    public List<Sprite> SoDamagedPig;
 
-    // Update is called once per frame
-    void Update()
-    {
+    private int timer;
+    private List<Sprite> nowSprites;
+    private short imageNumNow;
+
+	private void Start()
+	{
         
+	}
+	// Update is called once per frame
+	void Update()
+    {
+        timer++;
+        if (timer >= Random.Range(1000, 10000))
+        {
+            timer = 0;
+            Timer_Elased();
+        }
+        if(ChangeCondition())
+		{
+            timer = 0;
+            imageNumNow = 0;
+            ChangeSprite();
+        }
     }
+	private bool ChangeCondition()
+	{        
+        if (this.Health > 66 && nowSprites != this.DefaultPig)
+        {
+            nowSprites = this.DefaultPig;
+            return true;
+        }    
+        else if (this.Health <= 66 && nowSprites == this.DefaultPig)
+        {
+            nowSprites = this.DamagedPig;
+            return true;
+        }
+        else if (this.Health <= 33 && nowSprites == this.DamagedPig)
+        {
+            nowSprites = this.SoDamagedPig;
+            return true;
+        }
+        else
+		{
+            return false;
+		}
+    }
+	private void Timer_Elased()
+	{
+        if (imageNumNow < nowSprites.Count - 1)
+		{
+            imageNumNow++;
+        }           
+        else
+		{
+            imageNumNow = 0;
+        }
+        ChangeSprite();
+    }
+    private void ChangeSprite() =>
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = nowSprites[imageNumNow];
 }
