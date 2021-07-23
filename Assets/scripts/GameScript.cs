a6455a3ab3db4807414239dcbda2e16d89a20f85
Slava <sh-e-95@mail.ru>
2021-07-23 22:56:53 +0500

    Add Game Script

--- Assets/scripts/GameScript.cs
+++ Assets/scripts/GameScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    public GameObject SelectedBird { get; set; }
+    public Queue<GameObject> Birds = new Queue<GameObject>();
+    public bool GameStart { get; private set;} = false;
+    public Vector2 StartLocation = default;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SelectedBird = Birds.Dequeue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 cour = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (GameStart)
+        {
+            if(!Input.GetMouseButtonDown(0))
+			{
+                SelectedBird.transform.position = StartLocation;
+			}
+        }
+
     }
+    public void ChangeGameConditional() => this.GameStart = !this.GameStart;
 }
